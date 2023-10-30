1a2d92e1d83404ea25778f374c8d8e6abaa37fc2
jsteckel2 <34736893+jsteckel2@users.noreply.github.com>
2023-10-30 19:10:42 +0100

    Autocommit and 2023_10_30_19_10_42

--- autocommit.bat
+++ autocommit.bat
@@ -4,7 +4,7 @@ cd C:\Users\Jan\AppData\Roaming\PrusaSlicer
 setlocal enabledelayedexpansion
 
 
-rem Get the current date and time, but do stuff
+rem Get the current date and time, but do 
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set datetime=%%I
 set datetime=!datetime:~0,4!_!datetime:~4,2!_!datetime:~6,2!_!datetime:~8,2!_!datetime:~10,2!_!datetime:~12,2!
 

04afa90958c9290a361d22b1d770ee0a2268a9ff
jsteckel2 <34736893+jsteckel2@users.noreply.github.com>
2023-10-30 18:45:40 +0100

    msg

--- autocommit.bat
+++ autocommit.bat
@@ -1,9 +1,12 @@
-git pull
-git add *
-git commit -m Autocommit
-git push
-
 echo %DATE%
 echo %TIME%
-set datetimef=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%
-echo %datetimef%
+set datetimef=%date:~-4%_%date:~3,2%_%date:~0,2%_%time:~0,2%_%time:~3,2%_%time:~6,2%
+
+
+set msg = Autocommit and %datetime%
+
+git pull
+git add *
+git commit -m msg
+git push
+

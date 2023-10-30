7fa58402ac6362941106f51fbbd2e3b8675d0b40
jsteckel2 <34736893+jsteckel2@users.noreply.github.com>
2023-10-30 19:00:59 +0100

    Autocommit and 2023_10_30_19_00_59

--- autocommit.bat
+++ autocommit.bat
@@ -1,16 +1,16 @@
 @echo off
 setlocal enabledelayedexpansion
 
-rem Get the current date and time
+
+rem Get the current date and time, but do stuff
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set datetime=%%I
 set datetime=!datetime:~0,4!_!datetime:~4,2!_!datetime:~6,2!_!datetime:~8,2!_!datetime:~10,2!_!datetime:~12,2!
+
 
 set a=Autocommit
 set msg=!a! and !datetime!
 
-echo %msg%
 
-git pull
 git add *
 git commit -m "%msg%"
 git push
